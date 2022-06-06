e2c089ced6c087b5b4a91f185cbf252854f28419
abel magicho <32877794+kembAB@users.noreply.github.com>
2022-06-06 16:32:38 +0200

    All MVCbasic assignments1,2,3

---
+++ MVCWebApp/Controllers/GuessingGameController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MVCWebApp.Models.GuessingGame;
+using MVCWebApp.Models;
+using System.Diagnostics;
+
+namespace MVCWebApp.Controllers
+{
+    public class GuessingGameController : Controller
+    {
+        //random number generating
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var model = new GuessingProperties();
+            model.GenerateRandomNumber();
+            return View(model);
+        }
+      
+        [HttpPost]
+        public IActionResult Index(GuessingProperties mymodel)
+        {
+
+            if (ModelState.IsValid)
+            {
+                mymodel.ShowResult = true;
+                var resul = new comparePlyerRandomNum();
+                ViewBag.Result = resul.GetResult(ref mymodel);
+            }
+            return View(mymodel);
+
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new Errror { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MVCWebApp/Models/GuessingGame/comparePlyerRandomNum.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MVCWebApp.Models;
+namespace MVCWebApp.Models.GuessingGame
+{
+    /// <summary>
+    /// Gets the proper Result value based on values entereed
+    /// </summary>
+    /// <param name="_myModel"></param>
+    /// <returns></returns>
+    public class comparePlyerRandomNum
+    {
+        public string GetResult(ref GuessingProperties _myModel)
+        {
+            try
+            {
+
+                string rsltsubstr = null;
+
+                 int difference = _myModel.randomNumber - _myModel.EnteredNum;
+                if (difference != 0)
+                {
+                    rsltsubstr = Compare(difference);
+
+                    
+                    return $" {rsltsubstr}";
+                }
+                else
+                {
+                    _myModel.Success = true;
+                    return "Congratulations ! your guess is correct";
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+        private string Compare(int diffrence)
+        {
+            
+            string diff = (diffrence > 0 ? "tip:go higer" : "tip:go lower");
+            return diff;
+        }
+
+
+    }
+}
